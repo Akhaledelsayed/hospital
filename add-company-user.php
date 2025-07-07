30e498464b3aa5f13b6214c61a6a985da84e6005
saramo123 <sarausama1001@gmail.com>
2025-07-07 13:02:27 +0300

    added feature x

--- add-company-user.php
+++ add-company-user.php
@@ -5,6 +5,7 @@ if (isset($_SESSION['role']) && isset($_SESSION['id']) && $_SESSION['role'] == "
 <!DOCTYPE html>
 <html lang="en">
 <head>
+	
 	<meta charset="UTF-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
 	<title>Add Company</title>
